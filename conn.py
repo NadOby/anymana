c47739db72c377ad2892da3ac190926149dc1302
Dror Kronstein <dror.kronstein@mobileye.com>
2015-01-28 16:41:03 +0200

    Changed a bit

--- conn.py
+++ conn.py
@@ -5,16 +5,28 @@ except ImportError:
     exit(1)
 import sys
 
-class Conn():
+class AbstractConnection(object):
 
-    def __init__(self, host, user, passwd, cmd="help"):
-        self.cmd = cmd
+    def __init__(self, host, user, passwd):
         self.host = host
         self.user = user
         self.passwd = passwd
 
+    def get_power_command(self, disposition):
+        raise NotImplemented()
 
-    def runcmd(self):
+    def power(self, disposition = None):
+        if disposition:
+            cmd = self.get_power_command(disposition)
+        else:
+            cmd = self.get_power_command("status")
+
+        self.runcmd(cmd)
+
+    def reset(self, soft = True):
+        raise NotImplemented()
+
+    def runcmd(self, cmd):
         conn = paramiko.SSHClient()
         conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
@@ -26,19 +38,24 @@ class Conn():
             print("Unexpected error:", sys.exc_info()[:])
             exit(1)
 
-        stderr, stdout, stdin = conn.exec_command(self.cmd)
+        stderr, stdout, stdin = conn.exec_command(cmd)
         print (stdout.readlines())
         for line in stdout.readlines():
             if line != string.whitespace:
                 print(line)
         conn.close()
 
-class HPCmd(Conn):
-    HP_COMMAND = {
-            "poweron":"power on"
-            "poweroff": "power off"
+class HPConnection(AbstractConnection):
+    POWER_CMD = { 
+                  "on":     "power on",
+                  "off":    "power on",
+                  "status": "power"
     }
-    def __init__(self, host, user, passwd, command):
-        cmd = HP_COMMAND[command]
-        super(HPCmd, self).__init__(host, user, passwd, cmd)
+        
+    def __init__(self, host, user, passwd):
+        super(HPCmd, self).__init__(host, user, passwd)
+
+    def get_power_command(self, disposition):
+        return POWER_CMD[disposition]
+
 
